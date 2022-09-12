0f20ea5b81a6ca695448dd452caf4abfbdfd0bd1
rinas <topi2frie3elec@gmail.com>
2022-09-12 15:16:45 +0900

    add key

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from linebot import WebhookParser
+from linebot.models import TextMessage
+from aiolinebot import AioLineBotApi
+from .secret.py import CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+line_api = AioLineBotApi(channel_access_token=os.environ.get("token"))
 
 
 app = FastAPI()
