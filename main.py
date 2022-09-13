d231abefad6e0999820e4c6fd76e654c8703cb1a
rinas <topi2frie3elec@gmail.com>
2022-09-13 18:55:28 +0900

    add code

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from linebot.models import TextMessage
 from aiolinebot import AioLineBotApi
 from .secret.py import CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET
 
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
 
-line_api = AioLineBotApi(channel_access_token=os.environ.get("token"))
+# APIクライアントとパーサーをインスタンス化
+line_api = AioLineBotApi(channel_access_token=CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(channel_secret=CHANNEL_SECRET)
 
 
 app = FastAPI()
 
 @app.get("/")
-async def index():
-    return {"Hello world":"Yes"}
+async def index(request:Request):
+    events = parser.parse((await request.body()).decode("utf-8"), request.headers.get("X-Line-Signature",""))
+    for ev in events:
+        await line_api.reply_message_async(ev.reply_token, TextMessage(text=f"You said: {ev.message.text}"))
+    return "OK"
