ca5d83d0deb7fb32d69f85f836616200aedc9033
rinas <topi2frie3elec@gmail.com>
2022-09-15 16:09:35 +0900

    change code

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI, Request, BackgroundTasks
+from fastapi import FastAPI, Request
 from secret import CHANNEL_ACCESS_TOKEN, CHANNEL_SECRET
-
+from linebot import WebhookParser
+from linebot.models import MessageEvent, TextMessage, TextMessage
+from aiolinebot import AioLineBotApi
 
 # APIクライアントとパーサーをインスタンス化
-# line_api = AioLineBotApi(channel_access_token=CHANNEL_ACCESS_TOKEN)
-# parser = WebhookParser(channel_secret=CHANNEL_SECRET)
+line_api = AioLineBotApi(channel_access_token=CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(channel_secret=CHANNEL_SECRET)
 
 
 app = FastAPI()
 
 @app.get("/")
-async def index(event):
-    headers = {"Authorization": f"Bearer {CHANNEL_ACCESS_TOKEN}"}
-    data = {"message": event}
-    requests.post("https://notify-api.line.me/api/notify",headers=headers,data=data,)
+async def index(request:Request):
+    events = parser.parse((await request.body()).decode("utf-8"), request.headers.get("X-Line-Signature"))
+
+    for ev in events:
+        await line_api.reply_message_async(ev.reply_token, TextMessage(text=f"You Said:{ev.message.text}"))
+    return "OK"
