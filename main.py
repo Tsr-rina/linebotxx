d383e84ea46d4e8ffa13aea131f9f5173272b925
rinas <topi2frie3elec@gmail.com>
2022-09-18 14:30:21 +0900

    add get

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET','POST'])
 def test():
     return "OK"
 
